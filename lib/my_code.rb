4de373fe9cebb560dae27607cca82cdd2c2d273a
Allison Johnson <allison.cuttler@gmail.com>
2019-10-06 15:14:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 def map_to_negativize(source_array)
   new_array = []
   index = 0
-  while index < source_arr.length do
+  while index < source_array.length do
     new_array << source_array[index] * -1
     index += 1
   end #end while
