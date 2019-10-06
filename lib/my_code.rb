429d761f7a7350ce8e045e42b117d7f082a6b46b
Allison Johnson <allison.cuttler@gmail.com>
2019-10-06 15:16:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ def map_to_negativize(source_array)
   end #end while
   new_array
 end
+
+def map_to_no_change(source_array)
+  new_array = []
+  index = 0
+  while index < source_array.length do
+    new_array << source_array[index]
+    index += 1
+  end #end while
+  new_array
+end
